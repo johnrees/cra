4442fbbd0d107ea4e860ac3d896de3dcffb72e3a
John Rees <john@bitsushi.com>
2021-08-30 13:42:05 +0100

    basic token list

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { SolanaWalletWrapper } from "./components/SolanaWalletWrapper";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
